71b01c1565a6c44b65b76eb2039b7fbccd6d0532
SadeghAT <sadegh.artivox@gmail.com>
2023-01-01 23:56:47 +0330

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function Active() {
+     const menu = document.querySelector(".menu");
+
+     menu.classList.toggle("active-display");
+     menu.classList.toggle("active");
+}
+function UnActive() {
+     const menu = document.querySelector(".menu");
+
+     menu.classList.toggle("active-display");
+     menu.classList.toggle("active");
+}
